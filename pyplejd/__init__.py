03795d1612c3472aae84740d3cb03ce323c5b3dd
Kristian <kristian.schiander@gmail.com>
2026-08-22 19:46:52 +0200

    Revert "Prime persistent Plejd mesh gateway once"
    This reverts commit 68ef856e4ac90cab6ffbdfbf228b8133af31b524.

--- pyplejd/__init__.py
+++ pyplejd/__init__.py
@@ -53,7 +53,6 @@ class PlejdManager:
         self.route_control_writes_directly = False
         self.reconnect_after_control_write = False
         self.fast_remote_control_flush = False
-        self.prime_gateway_connection = False
         self.control_confirmation_timeout = 1.5
         self.reauthenticate_before_reconnect = True
         self.reauth_confirmation_timeout = 1.0
